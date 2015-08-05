476646b0e7cf038354f273eac22b32f5cb3dc762
Eric Richardson <e@ericrichardson.com>
2015-08-05 09:08:41 -0400

    Strip out admin password bits that were intended for the old UI

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,14 +3,10 @@ default.streammachine.user    = "streammachine"
 default.streammachine.install_ffmpeg  = false
 default.streammachine.install_redis   = false
 
-default.streammachine.version = "0.5.0"
+default.streammachine.version = "0.5.1"
 
 default.streammachine.dir     = "/opt/streammachine"
 
 default.streammachine.mode    = "standalone"
 default.streammachine.databag = "streammachine"
 default.streammachine.config  = "default"
-
-# Default password is "admin"
-default.streammachine.admin_password = "sha1$4910774a$1$d9ad69e6b7be6dd2fd4767dda957ed47a582c7e3"
-
--- recipes/install_redis.rb
+++ recipes/install_redis.rb
@@ -8,20 +8,12 @@ case node['platform']
       key           "C7917B12"
       action        :add
     end
-    
+
     package "redis-server"
-    
+
   when "centos"
     package "redis"
-    
+
   else
-    
-end
 
-# -- Install an admin user -- #
-
-execute "install-admin-user" do
-  action    :run
-  command   "redis-cli hset users admin '#{node.streammachine.admin_password}'"
-  #only_if   { `redis-cli hexists users admin` == "0" }
 end
